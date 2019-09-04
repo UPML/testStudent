80f9d262a6db3dbac307d610c3c3f8ec6acca0b1
Gudkov Konstantin <kagudkov@gmail.com>
2019-09-04 09:56:43 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    std::cout << "Hi" << '\n';
+}
