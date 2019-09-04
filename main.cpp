8cb1046f3ddb4b8a93b1f60b37c9c3201ac34634
Gudkov Konstantin <kagudkov@gmail.com>
2019-09-04 10:39:36 +0300

    test with zeros

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <complex>
 #include <vector>
-#include <algorithm>
-#include <iterator>
 
 
 int main() {
+    std::cout << "Hi" << '\n';
+
     std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+    std::cin.tie(nullptr);
+    std::vector<int> zeros(5, 0);
+    for(auto z : zeros){
+        std::cout << z << '\n';
+    }
 
     std::cout << "Hi" << '\n';
 }
